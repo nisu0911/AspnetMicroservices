80e406ebca805d9ad343d5d3dc9cd9f2e073c803
Nischal Shakya <shakya.nischal0911@gmail.com>
2022-01-14 23:15:47 +0545

    added mediatR order query

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQuery : IRequest<List<OrdersVm>>
+    {
+        public string UserName { get; set; }
+        public GetOrderListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+}
