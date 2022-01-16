de5234690455ced037c6d59a1fefbcb0f744a520
Nischal Shakya <shakya.nischal0911@gmail.com>
2022-01-16 22:13:28 +0545

    added update delete order command with handler and validator

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Ordering.Application.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+            
+        }
+    }
+}
