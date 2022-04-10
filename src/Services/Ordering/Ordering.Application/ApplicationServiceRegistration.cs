e66238371b01bc5e696b49531f3a9b4d51029084
Nischal Shakya <shakya.nischal0911@gmail.com>
2022-04-10 13:07:23 +0545

    added application service registration

--- src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using Ordering.Application.Behaviour;
+using Ordering.Application.Behaviours;
 
 namespace Ordering.Application
 {
@@ -14,7 +14,7 @@ namespace Ordering.Application
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
 
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandlesExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
             return services;
